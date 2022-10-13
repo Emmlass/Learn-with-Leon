7c2e4bdb5bf0606a2df0c3bde95b1692d666dce6
Emmlass <74358820+Emmlass@users.noreply.github.com>
2022-10-13 23:53:42 +0100

    Create multiplyThroughArray.js

---
+++ multiplyThroughArray.js
@@ -0,0 +1,10 @@
+//Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+
+
+function multiply(arr){
+    let product = 1;
+    forEach(num => product *= num)
+    alert(product)
+}
+// example
+multiply([2,3,5])// 30
